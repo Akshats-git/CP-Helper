536daff7d0d6f66166e7d452a6bc367581b3ea43
Akshat <akshatgupta099@gmail.com>
2026-07-04 01:38:35 +0530

    Add Pair class and initial Solution implementation

---
+++ CustomClasses/pair.java
@@ -0,0 +1,10 @@
+class Pair
+{
+    int first;
+    int second;
+    Pair(int first,int second)
+    {
+        this.first=first;
+        this.second=second;
+    }
+}
---
+++ main.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Solution 
+{
+  public static void main(String args[]) 
+  {
+    Scanner sc = new Scanner(System.in);
+    int tc = sc.nextInt();
+    int MOD=998244353;
+    while (tc-- > 0) 
+    {
+      int n=sc.nextInt();
+      long a[]=new long[n];
+      for(int i=0;i<n;i++)
+      {
+        a[i]=sc.nextLong();
+      }
+      System.out.println();
+    }
+  }
+}
