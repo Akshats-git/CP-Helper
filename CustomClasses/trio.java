78c22d5c12429424711bf867aa1150424085eb6f
Akshat <akshatgupta099@gmail.com>
2026-07-06 23:26:37 +0530

    Add Trio class implementation

---
+++ CustomClasses/trio.java
@@ -0,0 +1,12 @@
+class Trio
+{
+    int first;
+    int second;
+    int third;
+    Trio(int first,int second,int third)
+    {
+        this.first=first;
+        this.second=second;
+        this.third=third;
+    }
+}
